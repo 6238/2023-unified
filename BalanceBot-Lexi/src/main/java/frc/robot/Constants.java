a440a1b538b73fd503f17c412698b3b5c75a32ed
ribasl-mvla <100025733@mvla.net>
2023-01-08 12:18:26 -0800

    Initial test code which I think works
    It should run for half a second forward and half a second backward

---
+++ BalanceBot-Lexi/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,8 @@
+public class Constants {
+    public final static int LEFT_LEADER_ID = 30;
+    public final static int LEFT_FOLLOWER_ID_ONE = 32;
+    public final static int LEFT_FOLLOWER_ID_TWO = 34;
+    public final static int RIGHT_LEADER_ID = 31;
+    public final static int RIGHT_FOLLOWER_ID_ONE = 33;
+    public final static int RIGHT_FOLLOWER_ID_TWO = 35;
+}
--- BalanceBot-Lexi/src/main/java/frc/robot/Robot.java
+++ BalanceBot-Lexi/src/main/java/frc/robot/Robot.java
@@ -7,24 +7,51 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
  * arcade steering.
  */
 public class Robot extends TimedRobot {
-  private final PWMSparkMax m_leftMotor = new PWMSparkMax(0);
-  private final PWMSparkMax m_rightMotor = new PWMSparkMax(1);
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
-  private final Joystick m_stick = new Joystick(0);
+  private final WPI_TalonFX talonLeftLeader = new WPI_TalonFX(Constants.LEFT_LEADER_ID);
+  private final WPI_TalonFX talonLeftFollowerOne = new WPI_TalonFX(Constants.LEFT_FOLLOWER_ID_ONE);
+  private final WPI_TalonFX talonLeftFollowerTwo = new WPI_TalonFX(Constants.LEFT_FOLLOWER_ID_TWO);
+  private final WPI_TalonFX talonRightLeader = new WPI_TalonFX(Constants.RIGHT_LEADER_ID);
+  private final WPI_TalonFX talonRightFollowerOne = new WPI_TalonFX(Constants.RIGHT_FOLLOWER_ID_ONE);
+  private final WPI_TalonFX talonRightFollowerTwo = new WPI_TalonFX(Constants.RIGHT_FOLLOWER_ID_TWO);
+  private final DifferentialDrive robotDrive = new DifferentialDrive(talonLeftLeader, talonRightLeader);
+  private final Joystick stick = new Joystick(0);
+
+  private double speed;
+  private double rotation;
+
+  private int startTime;
+  private int timeLimit = 1000;
+
+  public void setDrive(double speed, double rotation) {
+    this.speed = speed;
+    this.rotation = rotation;
+  }
 
   @Override
   public void robotInit() {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotor.setInverted(true);
+    talonLeftFollowerOne.follow(talonLeftLeader);
+    talonLeftFollowerTwo.follow(talonLeftLeader);
+    talonRightFollowerOne.follow(talonRightLeader);
+    talonRightFollowerTwo.follow(talonRightLeader);
+    talonLeftLeader.setInverted(true);
+    talonLeftFollowerOne.setInverted(true);
+    talonLeftFollowerTwo.setInverted(true);
+
+    startTime = new 
+    setDrive(1,0);
   }
 
   @Override
@@ -32,6 +59,37 @@ public class Robot extends TimedRobot {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    m_robotDrive.arcadeDrive(-m_stick.getY(), -m_stick.getX());
+    robotDrive.arcadeDrive(-stick.getY(), -stick.getX());
   }
-}
+
+  @Override
+  public void periodic() {
+    long timeElapsed = System.currentTimeMillis() - startTime;
+
+    robotDrive.arcadeDrive(-speed, -rotation);
+    SmartDashboard.putNumber("DriveSpeed", -speed);
+    SmartDashboard.putNumber("DriveRotation", rotation);
+
+    SmartDashboard.putNumber("leftMotorsEncoderVelocity", talonLeftLeader.getSelectedSensorVelocity(PID_ID) * 0.1);
+    SmartDashboard.putNumber("rightMotorsEncoderVelocity", talonRightLeader.getSelectedSensorVelocity(PID_ID) * 0.1);
+
+    SmartDashboard.putNumber("distanceDriven", getPosition());
+    if(timeElapsed > timeLimit/2) {
+      speed = -1
+    } else {
+      speed = 0;
+    }
+    rotation = 0;
+  }
+
+  @Override
+  public boolean isFinished() {
+    long timeElapsed = System.currentTimeMillis() - startTime;
+    return timeElapsed > timeLimit;
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+      drive.setDrive(0,0);
+      System.out.println("Done!");
+  }
