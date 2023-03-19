69323b433841df66e65b4a9c1ad79bcaef40d327
ribasl-mvla <100025733@mvla.net>
2023-03-19 13:07:10 -0700

    Removed unneeded folders

--- CVR_Code/src/main/java/frc/robot/Robot.java
+++ CVR_Code/src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -19,6 +20,8 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+
+  private DigitalInput IRSensor = new DigitalInput(9);
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -47,6 +50,7 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    SmartDashboard.putBoolean("IR Sensor", !IRSensor.get());
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
