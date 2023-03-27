c963db49fec2b1f034876ec13892025aa4d300cd
Dillon Anawalt <DillonAnawaltboyscouts1@gmail.com>
2023-03-27 12:39:48 -0700

    idk what "math.pair" import does. removing it anyways

--- CVR_Code/src/main/java/frc/robot/commands/ArmManualCommand.java
+++ CVR_Code/src/main/java/frc/robot/commands/ArmManualCommand.java
@@ -1,6 +1,5 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.Pair;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
